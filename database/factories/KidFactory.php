34f5271d645f80e120bb83732be3332a4700c6b0
Girard <ghislain.girard@032m1123-21370.invites.cssh.qc.ca>
2020-02-26 15:58:25 -0500

    migrations done

---
+++ database/factories/KidFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Kid;
+use Faker\Generator as Faker;
+
+$factory->define(Kid::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
